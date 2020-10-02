e68d8c89e013bf8d9a7439f2d10fe4cb1d19ce11
persuasive-animation-2602 <dorinatasevska@gmail.com>
2020-10-02 13:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function happyHolidays(){
   return "Happy holidays!"
 }// write your code below!
 function happyHolidaysTo(name){
-  return `Happy Hollidays, ${name}`
+  return `Happy holidays, ${name}`
 }
